ff0fe522706f3e41d4cf872b35f25f7a15f60586
collinma13 <78565543+collinma13@users.noreply.github.com>
2021-09-03 17:37:50 +1200

    comment added and variable inserted
    added a comment for clear instructions and labeling. added a new variable with a prompt question for users knowledge

--- guess-the-number.js
+++ guess-the-number.js
@@ -1,4 +1,8 @@
 // Your code goes in this file.
 function guessNumber() {
 let ramdomNum = Math.floor(Math.random() * 21);
+// reveal a variable 
+let guess; 
+guess = prompt("Enter a number between 0 and 20");
+ 
 }
