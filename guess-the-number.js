75e4d464e07450e9121179530bd82255a4a327f9
collinma13 <78565543+collinma13@users.noreply.github.com>
2021-09-21 13:50:06 +1200

    changed function to play game
    instead of guess number

--- guess-the-number.js
+++ guess-the-number.js
@@ -8,32 +8,37 @@ function getName() {
     alert('Hello ' + username + ' lets play guess the number')
 }
 // second function
+function playGame() {
+    let rerun = 'yes'
+    while (rerun == 'yes') {
+        // starts game with a alert message 
+        alert('You are playing guess the number. Pick a number between 1 and 20! ')
+        // grabs name from function 1 
+        getName()
 
-// third function
-function guessNumber() {
-    // starts game with a alert message 
-    alert('You are playing guess the number. Pick a number between 1 and 20! ')
-    // grabs name from function 1 
-    getName()
+        let randomNum = Math.floor(Math.random() * 20) + 1
+        alert(randomNum)
 
-    let randomNum = Math.floor(Math.random() * 20) + 1
-    alert(randomNum)
+        // 2+ variables
+        let maxTries = 3
+        let counter = 0
 
-    // 2+ variables
-    let maxTries = 3
-    let counter = 0
-
-    // while loop
-    // if counter 
-    while (counter <= maxTries) {
-        let guess = prompt("Enter a number between 1 and 20")
-        if (!isNaN(guess)) 
-        if (guess == randomNum) {
-            alert('correct ')
-            counter += maxTries + 1
-        } else {
-            counter += counter + 1 
-            alert('incorrect')
+        // while loop
+        // if counter 
+        while (counter <= maxTries) {
+            let guess = prompt("Enter a number between 1 and 20")
+            if (!isNaN(guess)) {
+                if (guess == randomNum) {
+                    alert('correct ')
+                    counter += maxTries + 1
+                } else {
+                    counter += counter + 1
+                    alert('incorrect')
+                }
+            }else {
+                alert('error!') 
+            }  
         }
+        rerun = prompt('do you want to play again? ')
     }
 }
